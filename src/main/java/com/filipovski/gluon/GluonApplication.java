b602bd105e903194a40dc96a6d2f4b4032d022ef
Darko Filipovski <darkofilipovski0@gmail.com>
2021-06-20 14:16:55 +0200

    Initial commit

---
+++ src/main/java/com/filipovski/gluon/GluonApplication.java
@@ -0,0 +1,13 @@
+package com.filipovski.gluon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GluonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GluonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filipovski/gluon/GluonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipovski.gluon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GluonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
