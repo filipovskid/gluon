b60bdce20ea7fdc7dbaa3463fb59e07ee7dd6b5d
Darko Filipovski <darkofilipovski0@gmail.com>
2021-07-24 13:14:15 +0200

    Initial abstract implementation of executor

---
+++ gluon-executor/src/main/java/com/filipovski/gluonexecutor/executor/AbstractExecutor.java
@@ -0,0 +1,9 @@
+package com.filipovski.gluonexecutor.executor;
+
+/**
+ * Executor base class that manages common activities.
+ */
+
+public abstract class AbstractExecutor implements Executor {
+
+}
