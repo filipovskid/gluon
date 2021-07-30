0fe0f8f90ca9f7500dc2729fe45acabc80796f41
Darko Filipovski <darkofilipovski0@gmail.com>
2021-07-30 14:38:59 +0200

    Implemented an Environment abstraction which is responsible for facilitating Task execution

---
+++ gluon-executor/src/main/java/com/filipovski/gluon/executor/environment/AbstractEnvironment.java
@@ -0,0 +1,13 @@
+package com.filipovski.gluon.executor.environment;
+
+/**
+ * Abstract base class for remote environments represented by executor drivers.
+ * This class unifies activities relevant for all {@link ExecutionEnvironment}s,
+ * such as monitoring and lifecycle management.
+ */
+
+// TODO: Introduce monitoring and heartbeats for remote environment.
+
+public abstract class AbstractEnvironment implements ExecutionEnvironment {
+
+}
---
+++ gluon-executor/src/main/java/com/filipovski/gluon/executor/environment/ExecutionEnvironment.java
@@ -0,0 +1,33 @@
+package com.filipovski.gluon.executor.environment;
+
+import com.filipovski.gluon.executor.task.Task;
+
+/**
+ * {@link ExecutionEnvironment} facilitates the execution of {@link Task}s.
+ * This environment manages remote executors and their execution environment.
+ *
+ * <p>Each {@link ExecutionEnvironment} manages a process where {@link Task}s are
+ * executed. The process can be running on any instance e.g. local machine,
+ * container or any environment which allows starting a process.</p>
+ */
+
+public interface ExecutionEnvironment {
+
+    /**
+     * Prepare the environment for task execution. This is the point where the
+     * process this environment represents can be started.
+     */
+    void open();
+
+    /**
+     * Execute the {@link Task} within an external process.
+     *
+     * @param task
+     */
+    void execute(Task task);
+
+    /**
+     * Release the resources taken by the {@link ExecutionEnvironment}.
+     */
+    void close();
+}
