0ef0d409aaada33f54e1850cad5914504a46e48d
Darko Filipovski <darkofilipovski0@gmail.com>
2021-08-04 19:18:08 +0200

    Implemented initial execution statement task and task dispatcher component

---
+++ gluon-server/src/main/java/com/filipovski/gluonserver/task/ExecutionStatementResult.java
@@ -0,0 +1,36 @@
+package com.filipovski.gluonserver.task;
+
+import com.filipovski.gluon.executor.task.TaskResult;
+
+import java.time.Duration;
+
+/**
+ * The result of executing an {@link ExecutionStatement} task.
+ *
+ * <p>This is often a handle to the underlying computation.</p>
+ */
+// TODO: Introduce and document the different executor output types that could be
+//       the result of task's execution.
+
+public class ExecutionStatementResult extends TaskResult {
+
+    private boolean hasErrors;
+
+    private String errorMessages;
+
+    private String output;
+
+    public ExecutionStatementResult(String taskId,
+                                    Duration runtimeDuration,
+                                    boolean hasFailed,
+                                    boolean hasErrors,
+                                    String errorMessages,
+                                    String output) {
+        super(taskId, runtimeDuration, hasFailed);
+
+        this.hasErrors = hasErrors;
+        this.errorMessages = errorMessages;
+        this.output = output;
+    }
+
+}
