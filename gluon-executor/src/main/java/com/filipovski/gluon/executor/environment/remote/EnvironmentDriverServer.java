e1dbaa15cbade4bd0673bd24542441fb127b67a9
Darko Filipovski <darkofilipovski0@gmail.com>
2021-09-09 14:24:06 +0200

    Introduced environment driver as an entry point for runtime environment and its executors

---
+++ gluon-executor/src/main/java/com/filipovski/gluon/executor/environment/remote/EnvironmentDriverServer.java
@@ -0,0 +1,73 @@
+package com.filipovski.gluon.executor.environment.remote;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.ServerServiceDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class representing a server listening for and dispatching incoming calls to
+ * {@link com.filipovski.gluon.executor.proto.EnvironmentRuntimeDriverServiceGrpc.EnvironmentRuntimeDriverServiceImplBase}
+ * implementations.
+ *
+ * <p>Note: This class might be extracted to a separate RPC server utility or
+ * completely replaced by an inplace server construction.</p>
+ */
+
+public class EnvironmentDriverServer {
+
+    private final Logger logger = LoggerFactory.getLogger(EnvironmentDriverServer.class);
+
+    private final int port;
+
+    private final Server server;
+
+    public EnvironmentDriverServer(BindableService bindableService, int port) {
+        this(ServerBuilder.forPort(port), bindableService,  port);
+    }
+
+    public EnvironmentDriverServer(ServerBuilder<?> serverBuilder,
+                                   BindableService bindableService,
+                                   int port) {
+        this.port = port;
+        this.server = serverBuilder.addService(bindableService).build();
+    }
+
+    public void start() throws IOException {
+        server.start();
+        logger.info("Driver server started listening on port [{}]", port);
+    }
+
+    public void awaitTermination() throws InterruptedException {
+        server.awaitTermination();
+    }
+
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return server.awaitTermination(timeout, unit);
+    }
+
+    public int getPort() {
+        return server.getPort();
+    }
+
+    public boolean isShutdown() {
+        return server.isShutdown();
+    }
+
+    public boolean isTerminated() {
+        return server.isTerminated();
+    }
+
+    public void shutdown() {
+        server.shutdown();
+    }
+
+    public void shutdownNow() {
+        server.shutdownNow();
+    }
+}
---
+++ gluon-executor/src/main/java/com/filipovski/gluon/executor/environment/remote/ExecutionEnvironmentDriver.java
@@ -0,0 +1,91 @@
+package com.filipovski.gluon.executor.environment.remote;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.filipovski.gluon.executor.proto.EnvironmentRuntimeDriverServiceGrpc;
+import com.filipovski.gluon.executor.proto.ExecutionPayload;
+import com.filipovski.gluon.executor.proto.RemoteExecutionResult;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+/**
+ * Executable entry point for a runtime environment.
+ *
+ * <p>This class facilitates communication between the environment and its
+ * remote counterpart by providing RPC endpoints.</p>
+ */
+
+public class ExecutionEnvironmentDriver
+        extends EnvironmentRuntimeDriverServiceGrpc.EnvironmentRuntimeDriverServiceImplBase {
+
+    private final String driverHost;
+
+    private final int driverPort;
+
+    private final String gluonServerHost;
+
+    private final int gluonServerPort;
+
+    private final EnvironmentDriverServer server;
+
+    private ExecutionEnvironmentDriver(String host, int port, String serverHost, int serverPort) {
+        driverHost = host;
+        driverPort = port;
+        gluonServerHost = serverHost;
+        gluonServerPort = serverPort;
+        server = new EnvironmentDriverServer(this, port);
+    }
+
+    private void start() throws IOException, InterruptedException {
+        this.server.start();
+        this.server.awaitTermination();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        EnvironmentDriverArgs driverArgs = parseArguments(args);
+        ExecutionEnvironmentDriver environmentDriver =
+                new ExecutionEnvironmentDriver(driverArgs.host,
+                        driverArgs.port,
+                    driverArgs.serverHost,
+                    driverArgs.serverPort
+                );
+        environmentDriver.start();
+    }
+
+    private static EnvironmentDriverArgs parseArguments(String[] args) {
+        EnvironmentDriverArgs driverArgs = new EnvironmentDriverArgs();
+        JCommander.newBuilder()
+                .addObject(driverArgs)
+                .build()
+                .parse(args);
+
+        return driverArgs;
+    }
+
+
+    @Override
+    public void execute(ExecutionPayload request, StreamObserver<RemoteExecutionResult> responseObserver) {
+        super.execute(request, responseObserver);
+    }
+
+    public static class EnvironmentDriverArgs {
+        @Parameter(names = { "--host", "-h" }, description = "Environment driver hostname")
+        public String host;
+
+        @Parameter(names = { "--port", "-p" }, description = "Environment driver port")
+        public Integer port;
+
+        @Parameter(names = { "--server-host", "-r" }, description = "Gluon server hostname")
+        public String serverHost;
+
+        @Parameter(names = { "--server-port", "-d" }, description = "Gluon server port")
+        public int serverPort;
+
+        @Parameter(names = "--env-id", description = "Environment identifier")
+        public String environmentId;
+
+        @Parameter(names = "--worker-id", description = "Worker node identifier")
+        public String workerId;
+    }
+}
