b18cf3a7fb294fb50e48e2f52c2be65cd896c618
Darko Filipovski <darkofilipovski0@gmail.com>
2021-10-24 08:39:32 +0200

    Added a shell script for preparing data necessary for building images. Added configuration used by python executor

---
+++ gluon-docker/container-images/prepare-container-data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+[ ! -e Dockerfile ] && echo "Must be in Dockerfile's directory"
+
+dockerfile_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+gluon_dir=${dockerfile_dir}/../..
+
+# Build and prepare gluon executor driver
+
+executor_dir=${gluon_dir}/gluon-executor
+driver_output_dir=${dockerfile_dir}/driver
+driver_jar=gluon-environment-driver--0.0.1-SNAPSHOT.jar
+
+cd $executor_dir
+mvn package install
+cp ${executor_dir}/target/gluon-executor-0.0.1-SNAPSHOT-jar-with-dependencies.jar ${driver_output_dir}/${driver_jar}
+
+# Build and prepare gluon python executor
+
+python_exec_dir=${gluon_dir}/gluon-python
+python_output_dir=${dockerfile_dir}/executors/python
+python_exec_jar=gluon-python-executor--0.0.1-SNAPSHOT.jar
+python_conf=${dockerfile_dir}/conf/python.conf
+
+mkdir -p $python_output_dir
+cd $python_exec_dir
+mvn package
+cp ${python_exec_dir}/target/gluon-python-0.0.1-SNAPSHOT-jar-with-dependencies.jar ${python_output_dir}/${python_exec_jar}
+cp $python_conf ${python_output_dir}/executor.conf
