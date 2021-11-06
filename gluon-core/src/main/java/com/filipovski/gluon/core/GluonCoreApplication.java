45ea2bba767d4980773ed6ff3e58f3c525e2f4f8
Darko Filipovski <darkofilipovski0@gmail.com>
2021-11-06 17:40:44 +0100

    Configured notebook service as an Eureka client

--- gluon-core/src/main/java/com/filipovski/gluon/core/GluonCoreApplication.java
+++ gluon-core/src/main/java/com/filipovski/gluon/core/GluonCoreApplication.java
@@ -2,7 +2,9 @@ package com.filipovski.gluon.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GluonCoreApplication {
 
